d3113eb648b8e9362eaab10413509ea806fb46b6
lukaszp23 <lukaszp23@gmail.com>
2021-11-19 19:11:38 +0100

    changes

--- arch.sh
+++ arch.sh
@@ -39,9 +39,18 @@ sed -i '/pl_PL.UTF-8/ s/#//' /etc/locale.gen
 echo "KEYMAP=pl" > /etc/vconsole.conf
 locale-gen
 
+echo "Changing root password."
+passwd
+
 echo "Setting hostname."
 echo "Kalarepa" > /etc/hostname
 
 echo "Installing GRUB."
 pacman -S grub
-grub-install --target=i386-pc /dev/sd1
+grub-install --target=i386-pc /dev/sda1
+
+echo "Generating GRUB config."
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Installing XORG."
+pacman -S xorg
