e5d8924cd33badcb496b74ad23ffa6d0e97b728c
lukaszp23 <lukaszp23@gmail.com>
2021-11-21 19:03:39 +0100

    change

--- arch.sh
+++ arch.sh
@@ -31,29 +31,4 @@ echo "Generating fstab."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo "Chroot"
-arch-chroot /mnt \
-
-echo "Setting time zone"; \
-ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime; \
-hwclock --systohc; \
-
-echo "Setting locale."; \
-sed -i '/pl_PL.UTF-8/ s/#//' /etc/locale.gen; \
-echo "KEYMAP=pl" > /etc/vconsole.conf; \
-locale-gen; \
-
-echo "Changing root password."; \
-passwd; \
-
-echo "Setting hostname."; \
-echo "Kalarepa" > /etc/hostname; \
-
-echo "Installing GRUB."; \
-yes | pacman -S grub; \
-grub-install --target=i386-pc /dev/sda1; \
-
-echo "Generating GRUB config."; \
-grub-mkconfig -o /boot/grub/grub.cfg; \
-
-echo "Installing XORG."; \
-yes | pacman -S xorg; \
+arch-chroot /mnt
