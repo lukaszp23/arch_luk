c240a584d160367348b5ba422274648a179e440f
lukaszp23 <lukaszp23@gmail.com>
2021-11-21 19:26:26 +0100

    change

--- after_chroot.sh
+++ after_chroot.sh
@@ -4,23 +4,25 @@ echo "Setting time zone"
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 hwclock --systohc
 
-echo "Setting locale."
+echo "Setting locale"
 sed -i '/pl_PL.UTF-8/ s/#//' /etc/locale.gen
 echo "KEYMAP=pl" > /etc/vconsole.conf
 locale-gen
 
-echo "Changing root password."
+echo "Changing root password"
 (echo 'root'; echo 'root') | passwd
 
-echo "Setting hostname."
+echo "Setting hostname"
 echo "Kalarepa" > /etc/hostname
 
-echo "Installing GRUB."
+echo "Installing GRUB"
 pacman -S grub --noconfirm
 grub-install --target=i386-pc /dev/sda
 
-echo "Generating GRUB config."
+echo "Generating GRUB config"
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Installing XORG."
+echo "Installing XORG"
 pacman -S xorg --noconfirm
+
+echo "Rebooting"
--- arch.sh
+++ arch.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 
-echo "Updating system clock."
+echo "Updating system clock"
 timedatectl set-ntp true
 
-echo "Creating swap partition."
+echo "Creating swap partition"
 sgdisk -n 2:0:+24GiB -t 0:8200 /dev/sda
 
-echo "Creating file system partition."
+echo "Creating file system partition"
 sgdisk -n 1:0:0 -t 0:8300 /dev/sda
 
-echo "Formating partition."
+echo "Formating partition"
 mkfs.ext4 /dev/sda1
 
-echo "Initializing swap."
+echo "Initializing swap"
 mkswap /dev/sda2
 
-echo "Mounting file system."
+echo "Mounting file system"
 mount /dev/sda1 /mnt
 
-echo "Enabling swap."
+echo "Enabling swap"
 swapon /dev/sda2
 
-echo "Enable parallel download."
+echo "Enable parallel download"
 sed -i '/ParallelDownloads/ s/#//' /etc/pacman.conf
 
-echo "Installing essential packages."
+echo "Installing essential packages"
 pacstrap /mnt base linux linux-firmware
 
-echo "Generating fstab."
+echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo "Move script file to /mnt/home"
